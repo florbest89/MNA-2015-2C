03d8d383c66fb0cd50acf999151d5ba5ec303a39
mvalverd <mvalverd@alu.itba.edu.ar>
2015-10-03 18:01:50 -0300

    Se creó el algoritmo de Laplace y el método Adjunto. Se implementó el calculo de determinante para la matriz pedida usando ambos metodos.

--- TPE1/Laplace.m
+++ TPE1/Laplace.m
@@ -1,2 +1,24 @@
-function determinant = Laplace(matrix)
+function determinant = Laplace(matrix, n)
+    
+    determinant = 0;
+    
+    %CASO BASE - Matriz de orden 2
+    if (n == 2)
+        determinant = det(matrix);
+        return;
+    end
+    
+    %PASO RECURSIVO - Calculo el adjunto y ejecuto laplace. 
+    %Se ejecuta por profundidad. Se acumula el determinante resultado de
+    %toda la rama de matrices que corresponden a cada columna. Luego avanza
+    %el loop y se prosigue con la siguiente. 
+    for col = 1 : n
+        
+        firstElement = matrix(1,col);
+        [sign, tempAdjunt] = Adjunt(matrix, col);
+            
+        %Ejecuto Laplace con el adjunto de orden n-1 
+        determinant = determinant + firstElement * (sign * Laplace(tempAdjunt, n-1));
+    end
+    
 end
